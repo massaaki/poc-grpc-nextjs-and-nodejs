a1c733406e629cc50ae425f2cbdad8d2116c13ec
massaaki <eu@massaaki.com>
2022-01-13 15:32:09 -0300

    feat: add chat service to server

--- grpc-chat/src/configs/grpc.ts
+++ grpc-chat/src/configs/grpc.ts
@@ -1,16 +1,36 @@
+import path from 'path';
 import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
 
+import { ProtoGrpcType as ChatType } from '../../../grpc-contracts/src/chat-contract/chat-contract'
 import env from './env';
+import { ChatRequest } from '../../../grpc-contracts/src/chat-contract/chatPackage/ChatRequest';
+import { ChatResponse } from '../../../grpc-contracts/src/chat-contract/chatPackage/ChatResponse';
+import chatImplementation from '../services/chatImplementation';
+
 
 export class GRcpServer {
+  private chatPackage;
+
+  constructor() {
+    const contractPath = path.resolve(__dirname, '..', '..', '..', 'grpc-contracts', 'src', 'chat-contract', 'chat-contract.proto');
+
+    const packageDeffinition = protoLoader.loadSync(contractPath)
+    const obj = (grpc.loadPackageDefinition(packageDeffinition) as unknown) as ChatType;
+    this.chatPackage = obj.chatPackage;
+  }
+
   private configGrcpServer(): grpc.Server {
     const server = new grpc.Server();
+
+    server.addService(this.chatPackage.Chat.service, chatImplementation);
 
     return server;
   }
 
   startServer() {
     const server = this.configGrcpServer();
+
     server.bindAsync(`0.0.0.0:${env.grcp.port}`, grpc.ServerCredentials.createInsecure(), (error, port) => {
       if (error) {
         console.log("Error to start the service", error);
